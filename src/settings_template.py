a2212973d7f0bd353993b54f233b6755e046e451
Bernhard Bablok <bablokb@gmx.de>
2026-09-02 13:10:57 +0200

    split app-configuration into common, MCU and Blinka/PyGame sections

--- src/settings_template.py
+++ src/settings_template.py
@@ -40,55 +40,54 @@ except:
     hw_config.gamut = "mono" # rgb16, rgb24, acep_7colour, spectra_6, gray_4
     hw_config.eink  = True   # True|False
 
-# app configuration   --------------------------------------------------------
+# disp_name should only be set for the Blinka/PyGame environment
+disp_name = os.getenv("TESSERAE_DISPLAY")
 
-# NOTE: this is the generic configuration section that works for all devices.
-#       Configuration on Systems running Blinka+PyGame is more generic, see
-#       the last code block in this file. The settings `name` are treated
-#       differently and therefore anything
-#       configured here will be overriden. So either set these values
-#       there or remove the relevant lines in the last code block.
+# app configuration (common section: MCU and Blinka/PyGame)   ----------------
+#                    also edit the specific section below!
 
 app_config = Settings()
 app_config.url       = "http://tesserae.local:8765"
-#app_config.name     = "Kitchen Dashboard"    # manual pretty name or
-#app_config.name     = board.board_id         # use this default
 app_config.debug     = False
 app_config.debug_api = False
-app_config.always_on = False          # use power-off or deep-sleep
 #app_config.run_interval = 60         # fallback run-interval
-
 #app_config.format   = "bmp"          # "bmp" or "png"
-#app_config.rotation =                # force server-side content rotation
-#app_config.mac = "02:00:00:AA:BB:CC" # override MAC autodetection
+
 #app_config.pairing_code =            # use pairing code
 #app_config.magic =                   # invalidates a stored token
 #app_config.token =                   # hardwire the token
 
-# time-table configuration: one entry for every day starting at Monday
-# Format: (h_start, h_end, h_interval), (m_start, m_end, m_interval)
-#         start/end are inclusive
-# Note: a defined time-table will use local scheduling, otherwise
-#       scheduling is based on server-side information (next_poll_s)
+# --- configuration on MCUs   ------------------------------------------------
 
-# This example will run Mo-Fr, 9-17 every 15 minutes
-# TT_START   = 0
-# TT_INT     = 15
-# app_config.time_table = [
-#   ((9,17,1),(TT_START,59,TT_INT)),
-#   ((9,17,1),(TT_START,59,TT_INT)),
-#   ((9,17,1),(TT_START,59,TT_INT)),
-#   ((9,17,1),(TT_START,59,TT_INT)),
-#   ((9,17,1),(TT_START,59,TT_INT)),
-#   (None, None),
-#   (None, None),
-#   ]
+if not disp_name:
+  app_config.name     = board.board_id         # use this default or a
+  #pp_config.name     = "Kitchen Dashboard"    # set pretty name manually
+  app_config.always_on = False          # use power-off or deep-sleep
 
-# --- emulation or PyGame appliance   ----------------------------------------
+  #app_config.rotation =                # force server-side content rotation
+  #app_config.mac = "02:00:00:AA:BB:CC" # override MAC autodetection
 
-# SEE NOTE ABOVE!
+  # time-table configuration: one entry for every day starting at Monday
+  # Format: (h_start, h_end, h_interval), (m_start, m_end, m_interval)
+  #         start/end are inclusive
+  # Note: a defined time-table will use local scheduling, otherwise
+  #       scheduling is based on server-side information (next_poll_s)
 
-disp_name = os.getenv("TESSERAE_DISPLAY")
+  # This example will run Mo-Fr, 9-17 every 15 minutes
+  # TT_START   = 0
+  # TT_INT     = 15
+  # app_config.time_table = [
+  #   ((9,17,1),(TT_START,59,TT_INT)),
+  #   ((9,17,1),(TT_START,59,TT_INT)),
+  #   ((9,17,1),(TT_START,59,TT_INT)),
+  #   ((9,17,1),(TT_START,59,TT_INT)),
+  #   ((9,17,1),(TT_START,59,TT_INT)),
+  #   (None, None),
+  #   (None, None),
+  #   ]
+
+# --- configuration for Blinka/PyGame runtime (emulation, pi-appliance)  -----
+
 if disp_name:
   # parse name (if complete config is provided from the environment)
   disp_name = disp_name.split(',')[0]
@@ -97,6 +96,7 @@ if disp_name:
   hostname = os.uname()[1].split(".")[0]   # macOS reports "host.local"
   app_config.app_name  = f"Tesserae-{disp_name}"
   app_config.name      = f"{hostname} with display {disp_name}"
+  app_config.always_on = True          # loop
 
   # create app_config.mac only when manually configured
   if getattr(hw_config, "mac", None):
